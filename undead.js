42d6a441c12dbc56e2a9b8a652bae5682bd1b320
Brendan <brendanschatz97@gmail.com>
2020-09-11 02:42:34 -0400

    initial learning of chrome extentions

---
+++ undead.js
@@ -0,0 +1,10 @@
+// window.onload = function () {
+
+//     const divs = document.getElementsByTagName('h1')
+
+//         ('h1:contains("Died")').parent().hide()
+// }
+
+
+
+$('h1:contains("Brendan Schatz")').parent().replaceWith("<div><h1>Hi</h1></div>")
