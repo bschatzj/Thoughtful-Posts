0d978328049d4f4397448e97cf50711b1407da3d
Brendan <brendanschatz97@gmail.com>
2020-09-12 23:13:49 -0400

    trying to figure out why facebook is overwriting content script

--- facebook.js
+++ facebook.js
@@ -1,6 +1,14 @@
-window.onload()
 
-$("span:contains(' mind?')").parent().parent().replaceWith("<div><button id='buttonOverlay'>Are you sure you want to post?</button></div>")
-$("span:contains('your mind,')").parent().parent().parent().parent().parent().parent().parent().replaceWith("<div><button id='buttonOverlay'>Are you sure you want to post?</button></div>")
 
-$("form").parent().parent().replaceWith("<div><button id='buttonOverlay'>Are you sure you want to post?</button></div>")
+$("span:contains(' mind?')").parent().parent().replaceWith("<div><button onclick='alert('hi)' id='buttonOverlay'>Are you sure you want to post?</button></div>")
+
+
+const myButton = Document.getElementById("buttonOverlay")
+
+
+function myAlert() { alert('hi') }
+//("span:contains('your mind,')").parent().parent().parent().parent().parent().parent().parent().replaceWith("<div><button id='buttonOverlay'>Are you sure you want to post?</button></div>")
+
+//$("div:contains('Comment')").parent().parent().replaceWith("<div><button id='buttonOverlay'>Are you sure you want to post?</button></div>")
+
+//$("div:contains('Comment')").replaceWith("<div><button id='buttonOverlay'>Are you sure you want to post?</button></div>")
